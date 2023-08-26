c3e98706338f5562d565edccdc84c8bc609a1531
GITHUBFULLNAME <GITHUBEMAIL>
2023-08-26 12:50:44 -0600

    typewriter update

--- plugin.js
+++ plugin.js
@@ -1,24 +1,38 @@
 /*****************************************************************
-** Author: TBD
+** Author: I'd raather not say ;-)
 **
 ** A plugin adding Typed.js typerwriter text effect to Reveal.js presentations.
 **
 ** Version: 0.1
-** 
+**
+** Portions borrowed from Martijn De Jongh (Martino), martijn.de.jongh@gmail.com
+** https://github.com/Martinomagnifico
 ******************************************************************/
+
 window.RevealTypewriter = window.RevealTypewriter || {
     id: 'RevealTypewriter',
-    init: function(deck) {
+    init: function (deck) {
         initRevealTypewriter(deck);
     }
 };
 
-const initRevealTypewriter = function(Reveal){
-	var config = Reveal.getConfig().typewriter || {};
-
-    var typed = new Typed('#typed', {
-        stringsElement: '#typewriter-text', config
-    });
-
-	return this;
+let initRevealTypewriter = function (Reveal) {
+    const defaultOptions = {
+        stringsElement: '#typewriter-text',
+        typeSpeed: 70,
+        showCursor: true,
+        cursorChar: '_',
+        autoInsertCss: true
+    };
+    let options = Reveal.getConfig().typewriter || {};
+    let defaults = function defaults(options, defaultOptions) {
+        for (var i in defaultOptions) {
+            if (!options.hasOwnProperty(i)) {
+                options[i] = defaultOptions[i];
+            }
+        }
+    };
+    defaults(options, defaultOptions);
+    console.log(options);
+    var typed = new Typed('#typed', options);
 };
